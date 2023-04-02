2aad43560a5303dfac1eecb3bc20e3521cb8923a
KOHLI <sachin.kohli@accenture.com>
2023-04-02 16:41:14 +0530

    updated

---
+++ main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  2 00:15:47 2023
+
+@author: sachin.kohli
+"""
+
+import streamlit as st
+
+st.set_page_config(page_title="My Streamlit App")
+
+st.title("Welcome to my app")
+
+st.write("This is a simple example of a Streamlit app.")
+
+st.write("Here's an image of a cute cat:")
+
+# image = "https://www.rd.com/wp-content/uploads/2021/01/GettyImages-1151096937.jpg"
+# st.image(image, caption="A cute cat")
+
+st.write("Thanks for visiting!")
